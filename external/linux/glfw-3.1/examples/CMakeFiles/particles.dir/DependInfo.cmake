e2a5555902730be6837aebc1fa62bf1508aa64cd
Ariel Bernal <ariel.j.bernal@intel.com>
2015-03-02 09:28:13 -0800

    added linux

---
+++ external/linux/glfw-3.1/examples/CMakeFiles/particles.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/deps/getopt.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/examples/CMakeFiles/particles.dir/__/deps/getopt.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/deps/tinycthread.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/examples/CMakeFiles/particles.dir/__/deps/tinycthread.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/examples/particles.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/examples/CMakeFiles/particles.dir/particles.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "glfw-3.1-src/include"
+  "glfw-3.1-src/deps"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/context.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/glx_context.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/init.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/input.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/linux_joystick.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/monitor.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/posix_time.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/posix_tls.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/posix_tls.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/window.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/x11_init.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/x11_monitor.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/x11_window.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/glfw-3.1-src/src/xkb_unicode.c" "/home/ajbernal/dev/bat/research/external/linux/glfw-3.1/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "glfw-3.1-src/src"
+  "src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
