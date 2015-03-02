bd1cbc1b33f5d08940bb79d5fa3c0ee8c4a54899
Ariel Bernal <arielbernal@gmail.com>
2015-03-01 20:39:50 -0800

    testTweakAnt

--- test2013/main.cpp
+++ test2013/main.cpp
@@ -1,97 +1,89 @@
-//Include GLEW
 #include <GL/glew.h>
-
-//Include GLFW
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <cmath>
+#include <AntTweakBar.h>
 
-//Include the standard C++ headers
-#include <stdio.h>
-#include <stdlib.h>
-
-//Define an error callback
-static void error_callback(int error, const char* description)
-{
-  fputs(description, stderr);
-  _fgetchar();
+void EventMouseButtonGLFW3(GLFWwindow* window, int button, int action, int mods) {
+  if (TwEventMouseButtonGLFW(button, action)) return;
+  std::cout << "EventMouseButtonGLFW3\n";
+}
+void EventMousePosGLFW3(GLFWwindow* window, double xpos, double ypos) {
+  if (TwMouseMotion(int(xpos), int(ypos))) return;
+  std::cout << "EventMousePosGLW3\n";
+}
+void EventMouseWheelGLFW3(GLFWwindow* window, double xoffset, double yoffset) {
+  if (TwEventMouseWheelGLFW(yoffset)) return;
+  std::cout << "EventMouseWheelGLFW3\n";
+}
+void EventKeyGLFW3(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  if (TwEventKeyGLFW(key, action)) return;
+  std::cout << "EvenKeyGLFW3\n";
+}
+void EventCharGLFW3(GLFWwindow* window, int codepoint) {
+  if (TwEventCharGLFW(codepoint, GLFW_PRESS)) return;
+  std::cout << "EventCHarGLFW3\n";
+}
+void WindowSizeGLFW3(GLFWwindow* window, int width, int height) {
+  TwWindowSize(width, height);
 }
 
-//Define the key input callback
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, GL_TRUE);
-}
 
-int main(void)
+int main()
 {
-  //Set the error callback
-  glfwSetErrorCallback(error_callback);
-
-  //Initialize GLFW
-  if (!glfwInit())
-  {
-    exit(EXIT_FAILURE);
+  if (!glfwInit()) {
+    std::cerr << "Error initializing glfw...\n";
+    exit(-1);
   }
 
-  //Set the GLFW window creation hints - these are optional
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Request a specific OpenGL version
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //Request a specific OpenGL version
-  glfwWindowHint(GLFW_SAMPLES, 4); //Request 4x antialiasing
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
-  //Declare a window object
-  GLFWwindow* window;
-  
-
-  //Create a window and create its OpenGL context
-  window = glfwCreateWindow(640, 480, "Test Window", NULL, NULL);
-  glfwSetWindowPos(window, 100, 100);
-
-  //If the window couldn't be created
-  if (!window)
-  {
-    fprintf(stderr, "Failed to open GLFW window.\n");
+  GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
+  if (!window) {
+    std::cerr << "Error creating window...\n";
     glfwTerminate();
-    exit(EXIT_FAILURE);
+    exit(-1);
   }
-
-  //This function makes the context of the specified window current on the calling thread. 
   glfwMakeContextCurrent(window);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL); // can be GLFW_CURSOR_HIDDEN
 
-  //Sets the key callback
-  glfwSetKeyCallback(window, key_callback);
+  // Initialize AntTweakBar
+  //TwInit(TW_OPENGL, NULL);
+  TwInit(TW_OPENGL_CORE, NULL);
 
-  //Initialize GLEW
-  GLenum err = glewInit();
+  // Create a tweak bar
+  TwBar *bar = TwNewBar("TweakBar");
+  TwWindowSize(800, 600);
+  TwDefine(" GLOBAL help='This example shows how to integrate AntTweakBar with GLFW and OpenGL.' "); // Message added to the help bar.
 
-  //If GLEW hasn't initialized
-  if (err != GLEW_OK)
-  {
-    fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-    return -1;
+  // Set GLFW event callbacks
+  glfwSetWindowSizeCallback(window, (GLFWwindowposfun)WindowSizeGLFW3);
+  glfwSetMouseButtonCallback(window, (GLFWmousebuttonfun)EventMouseButtonGLFW3);
+  glfwSetCursorPosCallback(window, (GLFWcursorposfun)EventMousePosGLFW3);
+  glfwSetScrollCallback(window, (GLFWscrollfun)EventMouseWheelGLFW3);
+  glfwSetKeyCallback(window, (GLFWkeyfun)EventKeyGLFW3);
+  glfwSetCharCallback(window, (GLFWcharfun)EventCharGLFW3);
+
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_NO_ERROR) {
+    std::cerr << "Error initializing GLEW...\n";
+    glfwTerminate();
+    exit(-1);
   }
-
-  //Set a background color
-  glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
-
-  //Main Loop
-  do
-  {
-    //Clear color buffer
+  
+  while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
-
-    //Swap buffers
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, GL_TRUE);
+    float time = (float)glfwGetTime();
+    TwDraw();
     glfwSwapBuffers(window);
-    //Get and organize events, like keyboard and mouse input, window resizing, etc...
     glfwPollEvents();
-
-  } //Check if the ESC key had been pressed or if the window had been closed
-  while (!glfwWindowShouldClose(window));
-
-  //Close OpenGL window and terminate GLFW
-  glfwDestroyWindow(window);
-  //Finalize and clean up GLFW
+  }
+  TwTerminate();
   glfwTerminate();
-
-  exit(EXIT_SUCCESS);
+  return 0;
 }
