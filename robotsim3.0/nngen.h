a66b9334552672705706109f6b128823be2418d3
Ariel Bernal <arielbernal@gmail.com>
2015-10-13 21:58:26 -0700

    RRR

--- robotsim3.0/nngen.h
+++ robotsim3.0/nngen.h
@@ -11,10 +11,10 @@
 
 #define SIGMOID_A (1.715904709)
 #define SIGMOID_B (0.6666666667)
-#define SIGMOID_A2 (SIGMOID_A * SIGMOID_A)
+#define SIGMOID_A2 (SIGMOID_A *SIGMOID_A)
 #define SIGMOID_BA (SIGMOID_B / SIGMOID_A)
-#define DSIGMOID(S) (SIGMOID_BA * (SIGMOID_A2 - S * S))
-#define SIGMOID(x) (SIGMOID_A * tanh(SIGMOID_B * x))
+#define DSIGMOID(S) (SIGMOID_BA *(SIGMOID_A2 - S *S))
+#define SIGMOID(x) (SIGMOID_A *tanh(SIGMOID_B *x))
 //#define SIGMOID(x) (tanh(x))
 
 struct GENNeuron;
@@ -22,58 +22,71 @@ struct GENNeuron;
 struct GENSynapse {
   GENSynapse(GENNeuron *Input, double W) : Input(Input), W(W) {}
   GENNeuron *Input;
-  double W; // weight
+  double W;  // weight
 };
 
 struct GENNeuron {
-  enum { INPUT, OUTPUT, HIDDEN };
+  enum { INPUT, OUTPUT, EXCITATORY, INHIBITORY };
   GENNeuron(size_t id, size_t nntype, float x, float y, float z)
-      : id(id), nntype(nntype), x(x), y(y), z(z), alive(true), v(0),
-        theta(0), nu(0.9) {}
+      : id(id),
+        nntype(nntype),
+        x(x),
+        y(y),
+        z(z),
+        alive(true),
+        v(0),
+        theta(0),
+        nu(0.9) {}
 
   void addSynapse(GENNeuron *Neuron, double W) {
     Inputs.push_back(GENSynapse(Neuron, W));
   }
 
   void cleanUpInputs() {
-    Inputs.remove_if([](GENSynapse &S) { return S.Input->alive; });
+    Inputs.remove_if([](GENSynapse &S) { return !S.Input->alive; });
   }
 
   void update() {
-    double s = 0;
-    for (auto &e : Inputs)
-      s += e.W * e.Input->v;
-    s -= theta;
-    v = s < 0 ? 0 : s;
+    Ap = 0;
+    for (auto &e : Inputs) v += e.W * e.Input->Ap;
+    if (v > theta) {
+      Ap = 1;
+      v = H;
+    }
+    if (v > Rp) v = v - D < Rp ? Rp : v - D;
+    if (v < Rp) v = v + D > Rp ? Rp : v + D;
   }
 
   void updateSynapses() {
-    for (auto &e : Inputs) {
-      if (v != 0 && e.Input->v != 0)
-      double DW = nu * (1 - e.W) * v * e.Input->v;
-    }
   }
 
   size_t id;
   size_t nntype;
-  float x, y, z; // position
+  float x, y, z;  // Position
   bool alive;
-  double v;
-  double theta; // bias
-  double nu;
+  double v;      // Potential
+  double Ap;     // Action potential
+  double Rp;     // Resting potential
+  double H;      // Hyperpolarizing afterpotential
+  double D;      // Repolarization
+  double theta;  // Threshold
   std::list<GENSynapse> Inputs;
 };
 
 class GENNeuralNet {
-public:
+ public:
   GENNeuralNet(size_t NInput, size_t NOutput, size_t NHidden = 0,
                float XSize = 1, float YSize = 1, float ZSize = 1)
-      : NInput(NInput), NOutput(NOutput), NHidden(NHidden), XSize(XSize),
-        YSize(YSize), ZSize(ZSize) {
+      : NInput(NInput),
+        NOutput(NOutput),
+        NHidden(NHidden),
+        XSize(XSize),
+        YSize(YSize),
+        ZSize(ZSize) {
     initializeRandomNet();
   }
 
-protected:
+ protected:
   void initializeRandomNet() {
     // unsigned seed =
     // std::chrono::system_clock::now().time_since_epoch().count();
@@ -103,8 +116,7 @@ protected:
 
   void addHiddenNeuron() {
     auto uniform =
-        std::bind(std+
-          ::uniform_real_distribution<float>(0, 1), generator);
+        std::bind(std + ::uniform_real_distribution<float>(0, 1), generator);
 
     NHidden++;
     size_t id = Neurons.size();
@@ -119,19 +131,14 @@ protected:
   }
 
   void feed(const std::vector<double> &In) {
-    for (size_t i = 0; i < Input.size(); ++i)
-      Input[i]->v = In[i];
-    for (auto &e : Hidden)
-      e->update();
-    for (auto &e : Output) 
-      e->update();
+    for (size_t i = 0; i < Input.size(); ++i) Input[i]->v = In[i];
+    for (auto &e : Hidden) e->update();
+    for (auto &e : Output) e->update();
   }
 
-  void updateSynapses() {
+  void updateSynapses() {}
 
-  }
-
-private:
+ private:
   size_t NInput;
   size_t NOutput;
   size_t NHidden;
@@ -144,4 +151,4 @@ private:
   std::default_random_engine generator;
 };
 
-#endif // FFNN3L_H
+#endif  // FFNN3L_H
